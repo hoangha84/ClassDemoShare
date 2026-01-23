edb155b2f06368134195ff1b201fd93f2fb0188e
Thinkpad <hoangha84@gmail.com>
2026-01-23 17:27:58 +0700

    Done T04

---
+++ LTWeb02/252/C6/Tuan03/hello.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello</title>
+</head>
+<body>
+    <?php 
+        // phpinfo();
+        # echo "Hello" + " World";
+        echo "5" + "1000";
+        EcHo "<br>Hello"." World!";
+        $x = "<br>Hello";
+        $X = "Bình";
+        // echo $x . " " . $X;
+        echo '$x (Xin chào) $X';
+        echo "$x (Xin chào) $X";
+    ?>
+</body>
+</html>
